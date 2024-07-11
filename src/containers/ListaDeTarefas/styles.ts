0795b177150fdbb97ce7b162232b7e43702f5681
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-07-11 01:48:40 -0300

    Criando tarefas pt1

--- src/containers/ListaDeTarefas/styles.ts
+++ src/containers/ListaDeTarefas/styles.ts
@@ -1,15 +1,1 @@
-import styles from 'styled-components'
-
-export const Container = styles.main`
-  padding: 0 40px;
-  height: 100vh;
-  overflow-y: scroll;
-`
-
-export const Resultado = styles.p`
-display: block;
-margin-top: 40px;
-margin-bottom: 40px;
-font-size: 18px;
-font-weight: bold;
-`
+export {}
